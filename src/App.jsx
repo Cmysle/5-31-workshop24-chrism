06c4ccd0843f29b6437d3bfaef99c800abaea31d
Cmysle <Cmysle@yahoo.com>
2023-06-01 02:31:44 -0400

    3rd update

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,39 @@ import "./App.css";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleClick() {}
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <div className="App">
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+            id="upperChart"
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+
+      <div id="lowerChart">
+        {featPupId && (
+          <div>
+            <h2 id="pupName">{featuredPup.name}</h2>
+            <ul>
+              <li id="pupAge">Age: {featuredPup.age}</li>
+              <li id="pupEmail">Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
